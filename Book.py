c27babb1deecb81f834aefb9df5b910578839b6d
Samet Demirtuğ <149168462+samet-rum@users.noreply.github.com>
2024-06-11 21:41:02 +0200

    Book.py

---
+++ Book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, pages, type, author, isbn):
+        self.title = title
+        self.pages = pages
+        self.type = type
+        self.author = author
+        self.isbn = isbn
+
