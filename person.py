ae169a347754059deae2c0ae6311af53364ccaa8
Samet Demirtuğ <149168462+samet-rum@users.noreply.github.com>
2024-06-11 22:08:36 +0200

    person.py

---
+++ person.py
@@ -0,0 +1,8 @@
+
+class Person:
+    def __init__(self, name, person_id):
+        self.name = name
+        self.person_id = person_id
+
+    def __str__(self):
+        return f"Person(Name: {self.name}, ID: {self.person_id})"
